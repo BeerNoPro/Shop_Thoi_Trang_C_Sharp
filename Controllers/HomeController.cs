abaa5e5ba50e6556baa9847a8c116bc1d837a2e0
hung <phamviethungqnm@gmail.com>
2022-04-14 22:28:02 +0700

    1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopThoiTrang.Models;
+
+namespace ShopThoiTrang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ShopThoiTrangDBContext db = new ShopThoiTrangDBContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.SoMauTin = db.Products.Count();
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShopThoiTrang.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Roles { get; set; }
+        public int? Created_By { get; set; }
+        public DateTime? Created_At { get; set; }
+        public int? Updated_By { get; set; }
+        public DateTime? Updated_At { get; set; }
+        public int Status { get; set; }
+    }
+}
